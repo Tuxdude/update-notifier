5bdac9fe2b232e99908178fb386f5d4608730f16
Ash <tuxdude@StarScream>
2012-09-28 02:33:54 -0700

    Work-in-progress to try some gnome-shell stuff.
    Mostly example code posted on blog.fpmurphy.com

--- extension.js
+++ extension.js
@@ -1,10 +1,12 @@
 const Lang = imports.lang;
+const Mainloop = imports.mainloop;
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
 
-let text, button;
-
+const TOOLTIP_LABEL_SHOW_TIME = 0.15;
+const TOOLTIP_LABEL_HIDE_TIME = 0.1;
+const TOOLTIP_HOVER_TIMEOUT = 300;
 
 function UpdateNotifierExtension() {
     this._init();
@@ -19,11 +21,15 @@ UpdateNotifierExtension.prototype = {
                                    y_fill: false,
                                    track_hover: true });
         this.text = null;
+        this._hoverLabelTimeoutId = 0;
+        this._hoverLabel;
         let icon = new St.Icon({ icon_name: 'system-run',
                                  icon_type: St.IconType.SYMBOLIC,
                                  style_class: 'system-status-icon' });
         this.button.set_child(icon);
         this.button.connect('button-press-event', Lang.bind(this, this._showHello));
+        global.log('TEST LOG FROM ASH');
+//        this.button.connect('notify::hover', Lang.bind(this, this._onButtonHover));
     },
 
     _hideHello: function() {
@@ -51,11 +57,74 @@ UpdateNotifierExtension.prototype = {
                            onComplete: Lang.bind(this, this._hideHello) });
     },
 
+    _onButtonHover: function() {
+        if (this.button.get_hover()) {
+            if (this._hoverLabelTimeoutId == 0) {
+                this._hoverLabelTimeoutId =
+                    Mainloop.timeout_add(TOOLTIP_HOVER_TIMEOUT,
+                                         Lang.bind(this,
+                                                   function() {
+                                                       this.showLabel();
+                                                       return false;
+                                                   }));
+            }
+        }
+        else {
+            if (this._hoverLabelTimeoutId > 0)
+                Mainloop.source_remove(this._hoverLabelTimeoutId);
+            this._hoverLabelTimeoutId = 0;
+            this.hideLabel();
+        }
+    },
+
+    setLabelText: function(text) {
+        if (this._hoverLabel == null)
+            this._hoverLabel = new St.Label({ style_class: 'update-notifier-label' });
+        this._hoverLabel.set_text(text);
+        Main.layouManager.addChrome(this._hoverLabel);
+        this._hoverLabel.hide();
+    },
+    
+    showLabel: function() {
+        if (this._hoverLabel == null)
+            this.setLabelText('Just a sample hover text');
+        this._hoverLabel.opacity = 0;
+        this._hoverLabel.show();
+
+        let [stageX, stageY] = this.button.get_transformed_position();
+        let node = this._hoverLabel.get_theme_node();
+        let xOffset = node.get_length('-x-offset');
+        let yOffset = node.get_length('-y-offset');
+
+        let y = stageY + yOffset;
+        let x = stageX - (this._hoverLabel.get_width()) / 2 + xOffset;
+
+        this._hoverLabel.set_position(x, y);
+        Tweener.addTween(this._hoverLabel,
+                         { opacity : 255,
+                           time : TOOLTIP_LABEL_SHOW_TIME,
+                           transition : 'easeOutQuad'
+                         });
+    },
+
+    hideLabel: function() {
+        this._hoverLabel.opacity = 255;
+        Tweener.addTween(this,_hoverLabel,
+                         { opacity : 0,
+                           time : TOOLTIP_LABEL_HIDE_TIME,
+                           transition : 'easeOutQuad',
+                           onComplete: Lang.bind(this,
+                                                 function() { this._hoverLabel.hide(); } )
+                         });
+    },
+
     enable: function() {
+        global.log('enable');
         Main.panel._rightBox.insert_child_at_index(this.button, 0);
     },
 
     disable: function() {
+        global.log('disable');
         Main.panel._rightBox.remove_child(this.button);
     }
 };
